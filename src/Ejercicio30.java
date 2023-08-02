ced845422719beaca8f155f4e8bea0847220736b
yefry <yefryrodelo@gmail.com>
2023-08-02 15:02:56 -0500

    terminando ejercicios

--- src/Ejercicio30.java
+++ src/Ejercicio30.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 //30.Eliminar duplicados: Diseña un programa que elimine los elementos duplicados de un arreglo.
 public class Ejercicio30 {
     public static void main(String[] args) {
+        int[] array = {4,2,3,4,5,2,2,3,9,1,7,2,6,9,8,6,3,2,4};
+        Arrays.sort(array);
+        ArrayList<Integer> newArray = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            boolean duplicado = false;
+
+            for (int j = 0; j < newArray.size(); j++) {
+                if (array[i] == newArray.get(j)) {
+                    duplicado = true;
+                }
+            }
+
+            if(!duplicado) {
+                newArray.add(array[i]);
+            }
+        }
+
+        for (int num : newArray) {
+            System.out.print(num + " ");
+        }
 
     }
 }
---
+++ src/Ejercicio31.java
@@ -0,0 +1,7 @@
+// 31.Ordenar elementos: Implementa un algoritmo de ordenamiento (por ejemplo,el algoritmo de burbuja o el de selección) para ordenar un arreglo de enteros de manera ascendente.
+public class Ejercicio31 {
+    public static void main(String[] args) {
+        int[] array = {4,2,3,4,5,2,2,3,9,1,7,2,6,9,8,6,3,1,2,4};
+        
+    }
+}
