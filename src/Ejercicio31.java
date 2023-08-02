bad2f04ec97c202ad6cc5d764aa337c484d0afc7
yefry <yefryrodelo@gmail.com>
2023-08-02 15:29:55 -0500

    terminando ejercicios

--- src/Ejercicio31.java
+++ src/Ejercicio31.java
@@ -1,7 +1,28 @@
+import java.util.Arrays;
+
 // 31.Ordenar elementos: Implementa un algoritmo de ordenamiento (por ejemplo,el algoritmo de burbuja o el de selección) para ordenar un arreglo de enteros de manera ascendente.
 public class Ejercicio31 {
     public static void main(String[] args) {
+
         int[] array = {4,2,3,4,5,2,2,3,9,1,7,2,6,9,8,6,3,1,2,4};
-        
+        int cantidad = array.length;
+
+        for (int i = 0; i < cantidad - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < cantidad; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int intercambio = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = intercambio;
+        }
+
+        for (int number:array) {
+            System.out.println(number);
+        }
+
     }
 }
---
+++ src/Ejercicio32.java
@@ -0,0 +1,6 @@
+//32.Buscar un elemento: Escribe un programa que busque un número específico en un arreglo y muestre su índice (o un mensaje si no se encuentra).
+public class Ejercicio32 {
+    public static void main(String[] args) {
+
+    }
+}
